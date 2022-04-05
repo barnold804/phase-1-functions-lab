97937a6909b21fc8f4104754792d7a67c1144eee
Brian Arnold <b.arnold09@gmail.com>
2022-04-05 18:38:40 -0400

    commit tree ae837172646fd18c4db47bf63167b500463cd7b7

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distanceInBlocks) {
+    return Math.abs(distanceInBlocks - 42);
+}
+
+function distanceFromHqInFeet(distanceInFeet) {
+    return distanceFromHqInBlocks(distanceInFeet) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return ((Math.abs(start - destination) * 264))
+
+    // if (start < destination) {
+    //     return (destination - start) * 264;
+    // }
+    // else
+    //     return (start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    } 
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    } 
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far';
+    }
+}
